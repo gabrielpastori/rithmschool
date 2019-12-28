357d9e7e8ceac6a036f40211f07e35602dab604f
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2019-12-27 22:19:28 -0300

    Add Todo Exercise

--- javascript-fundamentals-solutions/todo_exercise/script.js
+++ javascript-fundamentals-solutions/todo_exercise/script.js
@@ -11,6 +11,7 @@ function createButton(src,hint,bt){
     bt.style.marginLeft="12px";
     return bt;
 }
+
 bt.addEventListener("click",function(){
         var input = document.querySelector("#insertInput").value;
         if(input!=""){
@@ -21,19 +22,33 @@ bt.addEventListener("click",function(){
             var li=document.createElement("li");
             var btDelete = document.createElement("input");
             var btComplete = document.createElement("input");
+            btDelete.classList+="Delete";
+            btComplete.classList+="Complete";
+            
             btDelete = createButton("delete.png","Delete task",btDelete);
             btComplete = createButton("complete.png","Mark as complete",btComplete);
+
             li.appendChild(span);
             li.appendChild(btDelete);
             li.appendChild(btComplete);
-           
+            btComplete.addEventListener("click",function(event){
+                var span = event.target.parentElement.querySelector("span");
+                span.style.textDecoration = "line-through";
+            }
+            );
+            btDelete.addEventListener("click",function(event){
+                var li = event.target.parentElement;
+                ul.removeChild(li);
+                
+            });
             ul.appendChild(li);
         }
     }
 );
-ul.addEventListener("click",function(){
+
+/*ul.addEventListener("click",function(){
     var li = event.target;
-    
-    console.log(li.parentElement.querySelector("span"));
+    console.log(event.target.classList[0]);
+    console.log(li.parentElement.querySelector("span").style.textDecoration = "line-through");
 }
-);
+);*/
