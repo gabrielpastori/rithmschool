b1857be4c114524744bcd6ee8da0835a4bb163ef
gabrielpastori <gabrielfigueira@alunos.utfpr.edu.br>
2020-01-03 14:03:54 -0300

    Add forEach, Map, and Filter exercises

--- javascript-fundamentals-solutions/javascript-iterators/forEach-map-filter.js
+++ javascript-fundamentals-solutions/javascript-iterators/forEach-map-filter.js
@@ -13,3 +13,38 @@ function addKeyAndValue(arr,key,value){
     return arr;
 }
 console.log(addKeyAndValue([{name: 'Elie'},{name: 'Tim'},{name: 'Elie'}], "isInstructor", true));
+
+function valTimesIndex(arr){
+    return arr.map(function(val,index){
+        return val*index;
+    });
+}
+console.log(valTimesIndex([1,2,3]));
+console.log(valTimesIndex([5,10,15]));
+
+function extractKey(arr,key){
+    return arr.map(function(val){
+        return val[key];
+    });
+}
+console.log(extractKey([{name: "Elie", isInstructor:true},{name: "Tim", isInstructor:true},{name: "Matt", isInstructor:true}], "name")
+);
+
+function filterLetters(arr,letter){
+    var count=0;
+    arr = arr.filter(function(val){
+        return val.toLowerCase()==letter.toLowerCase();
+    });
+    return arr.length;
+}
+console.log(filterLetters(["a","a","b","c","A"], "a"));
+console.log(filterLetters(["a","a","b","c","A"], "z"));
+console.log(filterLetters(["a","a","b","c","A"], "B"));
+
+function filterKey(arr,key){
+    return arr.filter(function(val){
+        return val[key]==true;
+    });
+}
+console.log(filterKey([{name: "Elie", isInstructor:true, isHilarious: false},{name: "Tim", isInstructor:true, isHilarious: true},{name: "Matt", isInstructor:true}], "isHilarious")
+);
